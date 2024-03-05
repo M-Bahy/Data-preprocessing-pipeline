cc34e8afece9f54c57451175c9a2df2b49046e5c
M-Bahy <Homelander.SolderBoy@gmail.com>
2024-03-05 22:06:50 +0200

    Add directory scanning functionality

--- post_veloview.py
+++ post_veloview.py
@@ -1,13 +1,23 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
+Working_Directory = os.getenv("WORKING_DIRECTORY")
+pcap_file_name = ""
+csv_file_names = []
 
 
+def scan_directory():
+    global pcap_file_name, csv_file_names
+    files = os.listdir(Working_Directory)
+    csv_file_names = [file for file in files if file.endswith(".csv")]
+    if not csv_file_names:
+        raise Exception("CSV file not found")
+    pcap_files = [file for file in files if file.endswith(".pcap")]
+    if not pcap_files:
+        raise Exception("PCAP file not found")
+    pcap_file_name = pcap_files[0]
 
 
-files = os.listdir(".")
-csv_files = [file for file in files if file.endswith(".csv")]
-if not csv_files:
-    raise Exception("CSV file not found")
-pcap_files = [file for file in files if file.endswith(".pcap")]
-if not pcap_files:
-    raise Exception("PCAP file not found")
-print(pcap_files)
+scan_directory()
+print(pcap_file_name)
